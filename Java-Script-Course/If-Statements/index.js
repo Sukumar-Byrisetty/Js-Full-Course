91d7138be657e1cb18b131386a5c0b3dbf65ada9
Sukumar-Byrisetty <byrisettysukumar@gmail.com>
2024-04-15 13:28:36 +0530

    last

--- Java-Script-Course/If-Statements/index.js
+++ Java-Script-Course/If-Statements/index.js
@@ -15,11 +15,39 @@
 //     console.log("you are a child!")
 // }
 
-let online = false;
+// let online = false;
 
-if(online) {
-    console.log("You are online!");
-}
-else {
-    console.log("You are offline!");
+// if(online) {
+//     console.log("You are online!");
+// }
+// else {
+//     console.log("You are offline!");
+// }
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+
+let age;
+
+mySubmit.onclick = function() {
+
+    age = myText.value;
+    age = Number(age);
+
+    if(age >= 100) {
+        resultElement.textContent = "You are TOO OLD to eneter this site";
+    }
+    else if (age == 0) {
+        resultElement.textContent = "You can't enter. you are not born yet";
+    }
+    if(age >= 18) {
+        resultElement.textContent = "You are old is enough to enter this site";
+    }
+    else if(age < 0) {
+        resultElement.textContent = "You are age can't be below 0";
+    }
+    else {
+        resultElement.textContent = "You must be 18+ to enter this site";
+    }
 }
